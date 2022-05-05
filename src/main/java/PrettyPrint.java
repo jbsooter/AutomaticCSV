ab082ed87a42777e57b1a58c098565205329e853
Blake Sooter <jbsooter@uark.edu>
2022-05-05 09:41:32 -0500

    Simplify prettyprint to just output objects in order
    Cleaner and more reliably readable

--- src/main/java/PrettyPrint.java
+++ src/main/java/PrettyPrint.java
@@ -1,32 +1,10 @@
-import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class PrettyPrint {
     public static void printAllArrayList(ArrayList arrayList) throws IllegalAccessException {
-        //grab fields
-        Field[] fields = arrayList.get(0).getClass().getDeclaredFields();
-
-        for(Field f: fields)
+        for(Object o: arrayList)
         {
-            f.setAccessible(true);
-            System.out.print(String.format("    %s    |", f.getName()));
-        }
-        //new row
-        System.out.println();
-
-        for(int i = 0; i < arrayList.size(); i++)
-        {
-            for(Field f: fields)
-            {
-                if(f.getType().equals(Double.class))
-                {
-                    System.out.print(String.format("        %.2f        |",f.get(arrayList.get(i))));
-                }
-                System.out.print(String.format("        %s        |",f.get(arrayList.get(i))));
-            }
-            //new row
-            System.out.println();
+            System.out.println(o);
         }
     }
 
@@ -36,30 +14,10 @@ public class PrettyPrint {
                 return;
         }
 
-        //grab fields
-        Field[] fields = arrayList.get(0).getClass().getDeclaredFields();
-        System.out.println(Arrays.toString(fields));
-        for(Field f: fields)
-        {
-            f.setAccessible(true);
-            System.out.print(String.format("    %s    |", f.getName()));
-
-        }
-        //new row
-        System.out.println();
 
         for(int i = 0; i < 5; i++)
         {
-            for(Field f: fields)
-            {
-                if(f.getType().equals(Double.class))
-                {
-                    System.out.print(String.format("        %.2f        |",f.get(arrayList.get(i))));
-                }
-                System.out.print(String.format("        %s        |",f.get(arrayList.get(i))));
-            }
-            //new row
-            System.out.println();
+            System.out.println(arrayList.get(i));
         }
 
 
@@ -70,17 +28,9 @@ public class PrettyPrint {
 
         for(int i = arrayList.size() - 5; i < arrayList.size(); i++)
         {
-            for(Field f: fields)
-            {
-                if(f.getType().equals(Double.class))
-                {
-                    System.out.print(String.format("        %.2f        |",f.get(arrayList.get(i))));
-                }
-                System.out.print(String.format("        %s        |",f.get(arrayList.get(i))));
-            }
-            //new row
-            System.out.println();
+            System.out.println(arrayList.get(i));
         }
+
         }
     }
 
